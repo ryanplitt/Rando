c44bff6b7805acad0c6c998fe103518f1946c178
Ryan Plitt <ryanplitt@gmail.com>
2017-02-27 09:19:31 -0700

    added storyboard files & outlets

---
+++ Rando/NumberCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  NumberCollectionViewCell.swift
+//  Rando
+//
+//  Created by Ryan Plitt on 2/27/17.
+//  Copyright © 2017 Ryan Plitt. All rights reserved.
+//
+
+import UIKit
+
+class NumberCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    
+}
